db28805ebee4a5cc9d1bf0745ddb36fc65eeab77
Anna Lee <leeanna@berkeley.edu>
2013-09-22 20:39:38 +0200

    first draft of r2

--- lab3/vertex.py
+++ lab3/vertex.py
@@ -15,6 +15,13 @@ class Vertex(object):
         count += 1
     return count
 
+  def getNeighbors(self, vertexList):
+    neighbors = []
+    for vertex in self.neighbors:
+      if vertex in vertexList:
+        neighbors.append(vertex)
+    return neighbors
+
 def createGraph(numVertices):
   """
   Creates an empty graph with numVertices vertices.
@@ -112,13 +119,70 @@ def r1(graph):
   
   return (max(setSize1 + 1, setSize2), count1 + count2 + 1)
 
+def r2(graph):
+  """
+  Returns (setSize, numRecursiveCalls)
+  """
+  if not graph:
+    return (0, 1)
+  
+  g = graph.copy()
+
+  i = findVertexOfDegree(2, graph)
+  if i is not None:
+    v = g.pop(i)
+    neighbors = v.getNeighbors(graph)
+    u = neighbors[0]
+    w = neighbors[1]
+    if u not in graph[w].neighbors:
+      global numVertices
+      neighbors = g[u].getNeighbors(g)
+      for neighbor in g[w].getNeighbors(g):
+        if neighbor not in neighbors:
+          neighbors.append(neighbor)
+      z = Vertex(numVertices + 1, neighbors)
+      numVertices += 1
+      g[z.num] = z
+
+    g.pop(u)
+    g.pop(w)
+    (setSize, count) = r2(g)
+    return (setSize + 1, count + 1)
+
+  i = findVertexOfDegree(1, graph)
+  if i is not None:
+    v = g.pop(i) # remove vertex i, which has only 1 neighbor
+    for j in v.neighbors:
+      g.pop(j, None) # remove vertex j without throwing an error
+    (setSize, count) = r2(g)
+    return (setSize + 1, count + 1) 
+
+  i = findVertexOfDegree(0, graph)
+  if i is not None:
+    g.pop(i)
+    (setSize, count) = r2(g)
+    return (setSize + 1, count + 1) 
+
+  i = findVertexOfMaxDegree(graph)
+  v = g.pop(i)
+  (setSize2, count2) = r2(g)
+
+  for j in v.neighbors:
+    g.pop(j, None) # remove vertex j without throwing an error
+  (setSize1, count1) = r2(g)
+  
+  return (max(setSize1 + 1, setSize2), count1 + count2 + 1)
+
 def main():
+  global numVertices
   numVertices = int(sys.argv[1])
   graph = readGraph(numVertices)
   if sys.argv[2] == "r0":
     (setSize, numCalls) = r0(graph)
   elif sys.argv[2] == "r1":
     (setSize, numCalls) = r1(graph)
+  else:
+    (setSize, numCalls) = r2(graph)
   print "setSize: ", setSize, "numCalls: ", numCalls
 
 if __name__ == "__main__":
